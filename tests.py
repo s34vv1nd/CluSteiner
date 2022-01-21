7ac95cb9ef83dc2654906bc70ba5e1a849a3e902
Hai Phong <aquariuslee99@gmail.com>
2022-01-21 23:59:33 +0700

    change order of instances

--- tests.py
+++ tests.py
@@ -8,12 +8,12 @@ import sys
 
 if __name__ == '__main__':
   alg = sys.argv[1] if len(sys.argv) > 1 else 0
-  print(listdir("input_data"))
-  for seed in range(30):
-    for instype in listdir("input_data"):
-      INPUT_FOLDER = "input_data\{}".format(instype)
-      OUTPUT_FOLDER = "output_data\Type_1_Small"
-      pathlib.Path(OUTPUT_FOLDER).mkdir(parents=True, exist_ok=True)
-      for file in listdir(INPUT_FOLDER):
+  for instype in ['Type_1_Small', 'Type_1_Large', 'Type_5_Small', 'Type_5_Large', 'Type_6_Small', 'Type_6_Large', 'Type_2', 'Type_3_Large']:
+    print(instype)
+    INPUT_FOLDER = "input_data\{}".format(instype)
+    OUTPUT_FOLDER = "output_data\Type_1_Small"
+    pathlib.Path(OUTPUT_FOLDER).mkdir(parents=True, exist_ok=True)
+    for file in listdir(INPUT_FOLDER):
+      for seed in range(30):
         print(file, str(seed))
         subprocess.run(["pypy3", "./main.py", "{}\\".format(instype) + file, str(seed), str(alg)])    

c71138af077cfbc607dd6f79753fe7cc2ec82d2f
Hai Phong <aquariuslee99@gmail.com>
2022-01-22 00:00:32 +0700

    change instance types order

--- tests.py
+++ tests.py
@@ -8,7 +8,7 @@ import sys
 
 if __name__ == '__main__':
   alg = sys.argv[1] if len(sys.argv) > 1 else 0
-  for instype in ['Type_1_Small', 'Type_1_Large', 'Type_5_Small', 'Type_5_Large', 'Type_6_Small', 'Type_6_Large', 'Type_2', 'Type_3_Large']:
+  for instype in ['Type_1_Small', 'Type_1_Large', 'Type_6_Small', 'Type_6_Large', 'Type_5_Small', 'Type_5_Large', 'Type_2', 'Type_3_Large']:
     print(instype)
     INPUT_FOLDER = "input_data\{}".format(instype)
     OUTPUT_FOLDER = "output_data\Type_1_Small"
